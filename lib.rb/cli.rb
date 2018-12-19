4e6344a76247031e479d110b9f69c96f825d7967
Cheryl K <cherylkelson@icloud.com>
2018-12-19 20:11:29 +0000

    add code to method bodies in cli.rb

--- lib.rb/cli.rb
+++ lib.rb/cli.rb
@@ -17,12 +17,12 @@ class OprahsFavoriteThings::CLI
     puts "What favorites would you like more information on?"
     input = gets.strip
 
-    favorites =OprahsFavoriteThings::Favorites.find(input.to_i)
+    favorites = OprahsFavoriteThings::Favorites.find(input.to_i)
 
     print_favorites(favorites)
 
     puts ""
-    puts "Would you like ot see another favorite? Enter Y or N"
+    puts "Would you like to see another favorite? Please enter  Y or N"
 
     input = gets.strip.downcase
     if input == "y"
@@ -31,4 +31,25 @@ class OprahsFavoriteThings::CLI
       puts ""
       puts" Thank you. Have a great day."
       exit
+    else
+      puts ""
+      puts "I don't understand that answer."
+      start
+    end
+  end
+
+  def print_favorites(favorites)
+    puts ""
+    puts""
+    puts "Description:'#{favorite.description}"
+    puts "Cost: #{favorite.cost}"
+    puts "Retailer: #{favorite.retailer}"
+  end
+
+  def print_favorites(from_number)
+    puts ""
+    puts"-----Favorites #{from_number} - #{from_number + 4} -----"
+    puts ""
+    OprahsFavoriteThings::Favorites.all[from_number-1, 10].each.with_index(from_number) do |favorite, index|
+      puts "#{index}. #{{favorite.description} - '#{favorite.retailer}"
       
