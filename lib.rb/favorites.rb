c12fb4d777939507ce689fe4d2e203ea93b73c1f
Cheryl K <cherylkelson@icloud.com>
2018-12-20 18:42:57 +0000

    Build self.new_from_index_page method by defining terms

--- lib.rb/favorites.rb
+++ lib.rb/favorites.rb
@@ -1,15 +1,22 @@
 class OprahsFavoriteThings::Favorites
 
-  attr_accessors :description, :cost, :retailer
+  attr_accessors :description, :cost, :retailer # these are the three attributes to be extracted from site
 
-  @@all - []
+  @@all - [] # empty array in which to place favorites 
 
   def self.new_from_index_page(favorites)
-    self.new(favorite )
-    self << favorite
+    self = OprahsFavoriteThings # the self is the class
+    new = index.new  # new means each individual favorite item
+    new_from_index_page = favorites.new # this shows what each new item is
+    @index = index # each index item is equivalent to each item pulled from the index
+    @list = list(favorites) # the instance of the list is equivalent to the list which has each favorite passed in
+    list << favorites_from_index(favorites) # the list will have each favorite pulled from the index shovelled in
+    self << favorite # each favorite is also passed into the class
+    self.new # the new method of the class will be returned
+
   end
 
-  def initialize
+  def initialize(favorites)
 
   end
 
