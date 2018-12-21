faeba992a221a0318a4e81348301cb32d013679e
Cheryl K <cherylkelson@icloud.com>
2018-12-21 16:56:11 +0000

    Add require_relative for version

--- config.rb/environment.rb
+++ config.rb/environment.rb
@@ -1,5 +1,11 @@
 require 'nokogiri'
 require 'open-url'
+require 'pry'
+
+require_relative "..oprahs_favorites/version"
+module OprahFavorites
+
+  
 site = "https://www.goday.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654"
 
 page = Nokogiri::HTML(open(site))
