282dadc5b200eb3ff0623fd0453d3aadfa26867c
Cheryl K <cherylkelson@icloud.com>
2018-12-21 17:06:29 +0000

    Create def menu method in class CLI

--- config.rb/environment.rb
+++ config.rb/environment.rb
@@ -1,11 +1,19 @@
+require_relative "..oprahs_favorites/version" # Per Flatiron, put the version file first"
+
 require 'nokogiri'
 require 'open-url'
 require 'pry'
 
-require_relative "..oprahs_favorites/version"
+
 module OprahFavorites
 
-  
+
+  class MyError < StandardError # Create subclass of an exception. Source: Stack Overflow
+    def initialize(msg = "You've triggered a MyError")
+      super(msg)
+    end
+  end
+
 site = "https://www.goday.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654"
 
 page = Nokogiri::HTML(open(site))
