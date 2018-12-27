8aa9882364063eec484205f5eddb541485da5882
Cheryl K <cherylkelson@icloud.com>
2018-12-27 17:51:37 +0000

    Add title, description, price to 2nd and 3rd favorites

--- lib.rb/hardcodes.rb
+++ lib.rb/hardcodes.rb
@@ -1,6 +1,8 @@
 main title: project.css <h1 class="headline___CuovH f8 f9-m fw3 mb3 mt0 publico-hed lh-title">Oprah's Favorite Things 2018: Oprah's picks announced!</h1>
 #this h1 is header/main tag
 
+
+
 first item title: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>
 #first item title
 
@@ -9,11 +11,21 @@ first item description: project.css <a href="https://www.today.com/parents/holid
 
 first item price: project.css **
 
+
+
 second item title: project.css <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>#second item
 <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2> #second item's title
 
 second item description:  project.css ***
 <
+second item price:  project.css **
+
+
+
 third item title: project.css :a href="https://www.amazon.com/gp/product/B07HJJ92Z3?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>
 <h2 class="title___3Yk1c mb6 mt0 lh-none">Glamourpuss NYC Knitted Faux Fur Mitten</h2> #third item's title
 ")
+
+third item description: project.css **
+
+third item price: project.css **
