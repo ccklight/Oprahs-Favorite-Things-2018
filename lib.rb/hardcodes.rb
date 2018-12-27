197a30ce4a9d77ae622e8b28494068c21723bd7f
Cheryl K <cherylkelson@icloud.com>
2018-12-27 17:47:05 +0000

    Add details to hardcoded html scrape-title, desc, price

--- lib.rb/hardcodes.rb
+++ lib.rb/hardcodes.rb
@@ -1,16 +1,19 @@
-<h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>
+main title: project.css <h1 class="headline___CuovH f8 f9-m fw3 mb3 mt0 publico-hed lh-title">Oprah's Favorite Things 2018: Oprah's picks announced!</h1>
+#this h1 is header/main tag
+
+first item title: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>
 #first item title
 
-<a href="https://www.today.com/parents/holiday-family-matching-pajama-set-ideas-t117017" target="_blank" class=" vilynx_listened">matching family pajamas</a>
+first item description: project.css <a href="https://www.today.com/parents/holiday-family-matching-pajama-set-ideas-t117017" target="_blank" class=" vilynx_listened">matching family pajamas</a>
 # first item's description
 
-<a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>#second item
+first item price: project.css **
+
+second item title: project.css <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>#second item
 <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2> #second item's title
 
+second item description:  project.css ***
 <
-:a href="https://www.amazon.com/gp/product/B07HJJ92Z3?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>
+third item title: project.css :a href="https://www.amazon.com/gp/product/B07HJJ92Z3?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>
 <h2 class="title___3Yk1c mb6 mt0 lh-none">Glamourpuss NYC Knitted Faux Fur Mitten</h2> #third item's title
 ")
-
-<h1 class="headline___CuovH f8 f9-m fw3 mb3 mt0 publico-hed lh-title">Oprah's Favorite Things 2018: Oprah's picks announced!</h1>
-#this h1 is header/main tag
