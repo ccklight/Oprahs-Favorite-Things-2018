6c8d45954ccaba5061155081005d85a36f2bdc4f
Cheryl K <cherylkelson@icloud.com>
2018-12-27 18:13:46 +0000

    Add options for html for price and description

--- lib.rb/hardcodes.rb
+++ lib.rb/hardcodes.rb
@@ -9,23 +9,29 @@ first item title: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Ath
 first item description: project.css <a href="https://www.today.com/parents/holiday-family-matching-pajama-set-ideas-t117017" target="_blank" class=" vilynx_listened">matching family pajamas</a>
 # first item's description
 
-first item price: project.css **
+first item price: project.css <span class="ListicleProduct__priceBtn cost___1u0dk cost___1P-qJ ls-normal lh-none df items-center justify-center bg-transparent fw5 founders-mono">$<!-- -->10</span>
+# Possible html for product price of first item
+first item price: project. css <strong>1. Burt's Bees Holiday Family Pajamas, $10+, <a href="https://www.amazon.com/b?node=18188463011&amp;?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
 
 
 
 second item title: project.css <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>#second item
 <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2> #second item's title
 
-second item description:  project.css ***
+second item description: project.css <strong>2. APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers, $200, <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
 <
-second item price:  project.css **
+#Better <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>option for html for second item description:  project.css
 
+second item price: project.css <strong>2. APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers, $200, <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
+second item price: project.css Bettr HTML <span class="ListicleProduct__priceBtn cost___1u0dk cost___1P-qJ ls-normal lh-none df items-center justify-center bg-transparent fw5 founders-mono">$<!-- -->200</span>
 
 
 third item title: project.css :a href="https://www.amazon.com/gp/product/B07HJJ92Z3?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a>
 <h2 class="title___3Yk1c mb6 mt0 lh-none">Glamourpuss NYC Knitted Faux Fur Mitten</h2> #third item's title
 ")
 
-third item description: project.css **
+third item description: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">Glamourpuss NYC Knitted Faux Fur Mitten</h2>
 
-third item price: project.css **
+
+third item price: project.css <strong>3. Glamourpuss NYC Knitted Faux Fur Mitten, $78, <a href="https://www.amazon.com/gp/product/B07HJJ92Z3?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
+third item BETTER price: project.css <span class="ListicleProduct__priceBtn cost___1u0dk cost___1P-qJ ls-normal lh-none df items-center justify-center bg-transparent fw5 founders-mono">$<!-- -->78</span>
