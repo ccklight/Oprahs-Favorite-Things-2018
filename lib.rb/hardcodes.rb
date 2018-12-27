5f68ee1ac5c1618451682e114e409a9aab88eefb
Cheryl K <cherylkelson@icloud.com>
2018-12-27 18:37:35 +0000

    Correct html tags for 3 items' descriptions

--- lib.rb/hardcodes.rb
+++ lib.rb/hardcodes.rb
@@ -2,15 +2,17 @@ main title: project.css <h1 class="headline___CuovH f8 f9-m fw3 mb3 mt0 publico-
 #this h1 is header/main tag
 
 
-
-first item title: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>
-#first item title
+GOOD: first item title: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">Burt's Bees Holiday Family Pajamas</h2>
 
 first item description: project.css <a href="https://www.today.com/parents/holiday-family-matching-pajama-set-ideas-t117017" target="_blank" class=" vilynx_listened">matching family pajamas</a>
 # first item's description
 
+first item BETTER description: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">Burt's Bees Holiday Family Pajamas</h2>
+first item DETAILED DESCRIPTION: project.css <p class="">These adorable <a href="https://www.today.com/parents/holiday-family-matching-pajama-set-ideas-t117017" target="_blank" class=" vilynx_listened">matching family pajamas</a>, available in both blue and red, will make for some great photo ops this holiday season.</p>
+
 first item price: project.css <span class="ListicleProduct__priceBtn cost___1u0dk cost___1P-qJ ls-normal lh-none df items-center justify-center bg-transparent fw5 founders-mono">$<!-- -->10</span>
 # Possible html for product price of first item
+
 first item price: project. css <strong>1. Burt's Bees Holiday Family Pajamas, $10+, <a href="https://www.amazon.com/b?node=18188463011&amp;?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
 
 
@@ -21,6 +23,7 @@ second item title: project.css <a href="https://www.amazon.com/gp/product/B07FF7
 second item description: project.css <strong>2. APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers, $200, <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
 <
 #Better <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>option for html for second item description:  project.css
+second item BETTER DETAILED description: <p class="">Easily go from the gym to a nice lunch in these stylish sneakers, which come in seven different colors.</p>
 
 second item price: project.css <strong>2. APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers, $200, <a href="https://www.amazon.com/gp/product/B07FF7NVSM?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
 second item price: project.css Bettr HTML <span class="ListicleProduct__priceBtn cost___1u0dk cost___1P-qJ ls-normal lh-none df items-center justify-center bg-transparent fw5 founders-mono">$<!-- -->200</span>
@@ -31,7 +34,7 @@ third item title: project.css :a href="https://www.amazon.com/gp/product/B07HJJ9
 ")
 
 third item description: project.css <h2 class="title___3Yk1c mb6 mt0 lh-none">Glamourpuss NYC Knitted Faux Fur Mitten</h2>
-
+third item BETTER DETAILED description: project.css <p class="">Dress up any outfit with these playful, cozy faux fur mittens. Since one size fits most, these make great gifts for a range of people. Try pairing them with the <a href="https://www.amazon.com/gp/product/B07HJDQMV6?tag=118330-oprahfavoritethings-20" target="_blank">matching slippers</a>, also on Oprah's list.</p>
 
 third item price: project.css <strong>3. Glamourpuss NYC Knitted Faux Fur Mitten, $78, <a href="https://www.amazon.com/gp/product/B07HJJ92Z3?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a></strong>
 third item BETTER price: project.css <span class="ListicleProduct__priceBtn cost___1u0dk cost___1P-qJ ls-normal lh-none df items-center justify-center bg-transparent fw5 founders-mono">$<!-- -->78</span>
