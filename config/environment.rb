094b2d02507ce8c9cd26d340265f2a0527975c55
Cheryl K <cherylkelson@icloud.com>
2018-12-30 20:49:06 +0000

    Delete extraneous code in environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,25 +1,7 @@
-require_relative "..oprahs_favorites/version" # Per Flatiron, put the version file first"
+require_relative "../lib/OprahsFavoriteThings/version.rb"
+require_relative "../lib/OprahsFavoriteThings/cli.rb"
 
 require 'nokogiri'
-require 'open-url'
+require 'open-uri'
 require 'pry'
 require 'colorize'
-
-require_relative "../config/environment" 
-
-require_relative "oprahs_favorite_things_2018/version"
-require_relative "oprahs_favorite_things_2018/favorites"
-require_relative "oprahs_favorite_things_2018/cli"
-
-
-  class MyError < StandardError # Create subclass of an exception. Source: Stack Overflow
-    def initialize(msg = "You've triggered a MyError")
-      super(msg)
-    end
-  end
-
-site = "https://www.goday.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654"
-
-page = Nokogiri::HTML(open(site))
-
-page.css("ul li strong")[0].text
---
+++ lib/OprahsFavoriteThings/version.rb
@@ -0,0 +1,3 @@
+module OprahsFavoriteThings
+  VERSION = "0.1.0"
+end 
