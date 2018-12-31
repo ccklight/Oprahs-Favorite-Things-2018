d821633171a9b4fc69f6431076128e607051b6c0
Cheryl K <cherylkelson@icloud.com>
2018-12-31 20:35:22 +0000

    Comment out all code for Scraper class

--- lib/OprahsFavoriteThings/scraper.rb
+++ lib/OprahsFavoriteThings/scraper.rb
@@ -1,50 +1,50 @@
 class OprahsFavoriteThings::Scraper
 
-  def get_page
-    Nokogiri::HTML(open("https://www.today.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654"))
-  end
-
-
-  def get_favorites
-    self.get_page.css(".")
-  end
-
-
-  def scrape_favorites_index
-    html = open("https://www.today.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654") #grabs HTML that makes up landing page
-    doc = Nokogiri::HTML(html) #converts HtML string returned by open-uri method and converts to a Node set
-    #doc.css(".favorites").each do |favorites|
-        ****.text
-    self.get_page.css("<a href="https://www.amazon.com/b?node=18188463011&amp;?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a> #first item
-
-  end
-
-
-  def make_favorites
-    self.get_favorites.each do |f|
-      favorite = Favorite.new
-      favorite.title = <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>
-      favorite.description = post.css ("<p class="">Easily go from the gym to a nice lunch in these stylish sneakers, which come in seven different colors.</p>").text
-      favorite.cost = <strong>1. Burt's Bees Holiday Family Pajamas, $10+.css ().text[01]
-      favorite.retailer = <button class="ListicleProduct__vendorBtn vendor___2BDSG vendor___1RnZ5 justify-center lh-denone ls-normal pt0 pb0 pl5 df bw-1px items-center"><span class="icon___1DgK6 lh-copy f2 white icon icon-shop"></span><span class="vendorName___1o43U founders-mono f3 f4-m ttu white">Amazon</span></button>.css().text
-      favorite.image = <img src="https://media2.s-nbcnews.com/j/newscms/2018_45/1384009/holiday_family_pajamas_cf552b884374f83627d0eb3044f75f53.fit-720w.jpg" alt="">
-
-      Scraper.new.make_favorite
-      end
-  end
-
-
-  def print__favorites
-    self.make_favorites
-    Favorite.all.each do |favorite|
-      if favorite.description
-        puts "Description: '#{favorite.description}"
-        puts "Cost: #{favorite.cost}"
-        puts "Retail: #{favorite.retail}"
-
-    Scraper.new.print_favorites
-
-  end
+  # def get_page
+  #   Nokogiri::HTML(open("https://www.today.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654"))
+  # end
+  #
+  #
+  # def get_favorites
+  #   self.get_page.css(".")
+  # end
+  #
+  #
+  # def scrape_favorites_index
+  #   html = open("https://www.today.com/style/oprahs-favorite-things-2018-oprahs-picks-announced-t141654") #grabs HTML that makes up landing page
+  #   doc = Nokogiri::HTML(html) #converts HtML string returned by open-uri method and converts to a Node set
+  #   #doc.css(".favorites").each do |favorites|
+  #       ****.text
+  #   self.get_page.css("<a href="https://www.amazon.com/b?node=18188463011&amp;?tag=118330-oprahfavoritethings-20" target="_blank">Amazon</a> #first item
+  #
+  # end
+  #
+  #
+  # def make_favorites
+  #   self.get_favorites.each do |f|
+  #     favorite = Favorite.new
+  #     favorite.title = <h2 class="title___3Yk1c mb6 mt0 lh-none">APL: Athletic Propulsion Labs Women's Techloom Bliss Sneakers</h2>
+  #     favorite.description = post.css ("<p class="">Easily go from the gym to a nice lunch in these stylish sneakers, which come in seven different colors.</p>").text
+  #     favorite.cost = <strong>1. Burt's Bees Holiday Family Pajamas, $10+.css ().text[01]
+  #     favorite.retailer = <button class="ListicleProduct__vendorBtn vendor___2BDSG vendor___1RnZ5 justify-center lh-denone ls-normal pt0 pb0 pl5 df bw-1px items-center"><span class="icon___1DgK6 lh-copy f2 white icon icon-shop"></span><span class="vendorName___1o43U founders-mono f3 f4-m ttu white">Amazon</span></button>.css().text
+  #     favorite.image = <img src="https://media2.s-nbcnews.com/j/newscms/2018_45/1384009/holiday_family_pajamas_cf552b884374f83627d0eb3044f75f53.fit-720w.jpg" alt="">
+  #
+  #     Scraper.new.make_favorite
+  #     end
+  # end
+  #
+  #
+  # def print__favorites
+  #   self.make_favorites
+  #   Favorite.all.each do |favorite|
+  #     if favorite.description
+  #       puts "Description: '#{favorite.description}"
+  #       puts "Cost: #{favorite.cost}"
+  #       puts "Retail: #{favorite.retail}"
+  #
+  #   Scraper.new.print_favorites
+  #
+  # end 
 
 
 end
