0ca8bebf61f3a50a6e75e158fb8c3966a3d69ef8
Cheryl K <cherylkelson@icloud.com>
2019-01-01 18:13:36 +0000

    Add back / to environment.rb and add require_relative

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative "../lib/OprahsFavoriteThings/cli.rb"
 require_relative "../lib/OprahsFavoriteThings/favorites.rb"
 require_relative "../lib/OprahsFavoriteThings/scraper.rb"
 require_relative "../lib/OprahsFavoriteThings/hardcodes.rb"
+require_relative "../config/environment.rb"
 
 
 require 'nokogiri'
