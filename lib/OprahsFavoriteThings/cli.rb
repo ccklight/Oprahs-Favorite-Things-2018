181d299c89a13d3cbbda8fa2609898cd19af2adc
Cheryl K <cherylkelson@icloud.com>
2019-01-13 19:22:11 +0000

    Change .new.start to try to get list of favorites

--- lib/OprahsFavoriteThings/cli.rb
+++ lib/OprahsFavoriteThings/cli.rb
@@ -2,87 +2,82 @@ require 'pry'
 
 class OprahsFavoriteThings::CLI
 
-  def start
-    all_favorites = OprahsFavoriteThings::Favorites.all
-    input = nil
-    while input != "exit"
-    puts ""  
-    puts "Welcome to Oprah'2018 Favorite Things."
-     OprahsFavoriteThings::Favorites.new.start
+  OprahsFavoriteThings::Favorites.new.start
      a = OprahsFavoriteThings::Scraper.new
      a.welcome
      OprahsFavoriteThings::Scraper.scrape_favorites
-     #binding.pry
-
 
      all_favorites.each do |item|
-       binding.pry
+  #      binding.pry
        puts item.product
           end
     end
-
 
   def call
     OprahsFavoriteThings::Scraper.new.make_favorites
     make_favorites
     start
   end
-
-
-
-  #
   #   Scraper.new.scrape_index(favorites)
-
-  #   puts "Welcome to Oprah's Favorite Things 2018."
-  #   puts "What number of Oprah's favorite things would you like to see? 1-5, 6-10, 11-15?"
-  #   puts ""
-  #   input = gets.strip.to_i
-  #
-  #   print_favorites(input)
-  #
-  #   puts ""
-  #   puts "What favorites would you like more information on?"
-  #   input = gets.strip
-  #
   #   favorites = OprahsFavoriteThings::Favorite.find(input.to_i)
-  #
-  #   print_favorite(input)
-  #
-  #   puts ""
-  #   puts "Would you like to see another favorite? Please enter  Y or N"
-  #
-  #   input = gets.strip.downcase
-  #   if input == "y"
-  #     start
-  #   elsif input == "n"
-  #     puts ""
-  #     puts" Thank you. Have a great day."
-  #     exit
-  #   else
-  #     puts ""
-  #     puts "I don't understand that answer."
-  #     start
-  #     end
-  #   end
-  #  end
-  #
-  #
-  # def print_favorite(favorite)
-  #   puts ""
-  #   puts ""
-  #   puts "Description: #{favorite.description}"
-  #   puts "Cost: #{favorite.cost}"
-  #   puts "Retailer: #{favorite.retailer}"
-  # end
-  #
-  #
-  # def make_favorites(from_number)
-  #   puts ""
-  #   puts"-----Favorites #{from_number} - #{from_number + 4} -----"
-  #   puts ""
-  #   OprahsFavoriteThings::Favorite.all[from_number-1, 10].each.with_index(from_number) do |favorite, index|
-  #     puts "#{index}. #{favorite.description} - #{favorite.retailer}"
-  #       end
-  # end
+
+  def start
+    all_favorites = OprahsFavoriteThings::Favorites.all
+    input = nil
+    while input != "exit"
+    puts ""
+    puts "Welcome to Oprah'2018 Favorite Things."
+    puts "What number of Oprah's favorite things would you like to see? 1-5, 6-10, 11-15?"
+    puts ""
+
+    input = gets.strip.to_i
+
+    print_favorites(favorites[input])
+
+    puts ""
+    puts "What favorites would you like more information on?"
+
+    input = gets.strip.to_i
+
+    print_favorite(favorites[input])
+
+    puts ""
+    puts "Would you like to see another favorite? Please enter  Y or N"
+
+      input = gets.strip.downcase
+      if input == "y"
+        start
+      elsif input == "n"
+        puts ""
+        puts" Thank you. Have a great day."
+        exit
+      else
+        puts ""
+        puts "I don't understand that answer."
+        start
+        end
+      end
+     end
+
+
+
+     def print_favorite(favorite)
+       puts ""
+       puts ""
+       puts "Description: #{favorite.description}"
+       puts "Price: #{favorite.price}"
+       puts "Retailer: #{favorite.retailer}"
+     end
+
+
+
+      def make_favorites(from_number)
+         puts ""
+         puts"-----Favorites #{from_number} - #{from_number + 4} -----"
+         puts ""
+         OprahsFavoriteThings::Favorite.all[from_number-1, 10].each.with_index(from_number) do |favorite, index|
+           puts "#{index}. #{favorite.description} - #{favorite.retailer}"
+             end
+      end
 
 end
